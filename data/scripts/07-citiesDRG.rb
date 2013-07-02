65a1952ea7baa052065874afd769de9415345618
gelicia <kdurivage@gelicia.com>
2013-07-02 17:48:38 -0500

    Halfway though city DRG data

--- data/scripts/07-citiesDRG.rb
+++ data/scripts/07-citiesDRG.rb
@@ -1,3 +1,12 @@
+##this takes a while to run, it would benefit from querying once per city 
+## and splitting up the file creation from the result set
+
+##even if a city has one provider, I don't want so much logic in the presentation layer
+##because a multi-provider city can have a DRG that only one provider has done, and those are way different
+
+##also, most cities have one provider, we could maybe write something in the presentation layer 
+## to look for this data in the providernfile already generated in 04
+
 require 'mysql'
 require 'csv'
 
@@ -25,7 +34,32 @@ CSV.foreach("../national/DRG-All.csv", {:headers=>:first_row}) do |stateRow|
 		##iterate through each diagnosis
 		diagArray.each { 
 			|diagCode|
+##stopped in ohio, resume there
+			##create a file for that city's DRG data
+			config_path = File.expand_path("../../national/" + state + "/" + city + "/DRG-" + diagCode + ".csv", __FILE__)
+			outfile = File.open(config_path, 'w')
 
+			rs = con.query 'SELECT REPLACE(p.name, \',\', \'\') name, p.national_id, ROUND(avg(i.averageCoveredCharges)) averageCoveredCharges, ROUND(avg(i.averagePayments)) averagePayments
+				FROM provider p
+				INNER JOIN inpatient i ON i.providerID = p.id
+				INNER JOIN diagnosis d ON i.drgid = d.id
+				WHERE d.code = \'' + diagCode + '\' AND p.state = \'' + state + '\' AND p.city = \'' + city.gsub("'", "''") + '\'
+				group by p.name'
+
+			n_rows = rs.num_rows
+
+			if n_rows > 0
+				outfile.write("providerName,providerNationalID,avgCoveredCharges,averagePayments")
+
+		 		n_rows.times do
+		        	outfile.write("\n" + rs.fetch_row.join(","))
+		    	end
+
+				outfile.close
+			else
+				outfile.close
+				File.delete(config_path)
+			end
 		}
 	end
 	
