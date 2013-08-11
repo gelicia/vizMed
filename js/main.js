a19b5110c6ee418e41f0068c0b8dddf0ca2fa431
gelicia <kdurivage@gelicia.com>
2013-08-11 09:40:02 -0500

    Alright main2.js, you can graduate

--- js/main.js
+++ js/main.js
@@ -1,156 +1,244 @@
-//how the bar chart will look
 var barSpec = {spacing: 2, h: 20, fill: '#5777C0', moFill: '#A2B6E5'};
-//total margin all around the sides
-var margin = {t: 20, b: 10, l: 10, r: 10};
-//range for the chart
-var chartSpec = {topMargin: 30, w: 600, label:{size: 12, color: "#000", moColor: "#474747"}};
+var chartSpec = {topMargin: 100, w: 600, insideOffset: 30, label:{size: 12, color: "#000", moColor: "#474747"}};
 //todo: replace with something dynamic
-var svgTemp = {w: 1000, h: 5000};
+var svgTemp = {w: 1000};
 var transitionSpeed = 500;
 
-//this will be referenced by functions that need to transition from the old scale to a new one
-var prevState = {xScale: undefined};
+var travelPath = ["USA"];
 
 function loadData(){
-  //fill list of DRGs
-  d3.csv('./data/diagnoses.csv', function(error, csv){
-    var dropDown = d3.select('#drgSelect');
+	//fill list of DRGs
+	d3.csv('./data/diagnoses.csv', function(error, csv){
+		var dropDown = d3.select('#drgSelect');
 
-    csv.unshift({code: "All", description: "All DRGs"});
+		csv.unshift({code: "All", description: "All DRGs"});
 
-    dropDown.selectAll('option')
-    .data(csv).enter().append('option')
-    .attr("value", function(d){ return d.code;})
-    .text(function(d){return d.code + " - " + d.description;});
+		dropDown.selectAll('option')
+		.data(csv).enter().append('option')
+		.attr("value", function(d){ return d.code;})
+		.text(function(d){return d.code + " - " + d.description;});
+	});
 
-  });
+    //this can just be hardcoded, will always init at drg all 
+	d3.csv('./data/national/DRG-All.csv', function(error, csv){
+		var chartMain = d3.select("#chartMain")
+		.attr({
+			height: (csv.length * (barSpec.h + barSpec.spacing)) + 100, //todo this is messed up, why does the top margin need to be huge
+			width: svgTemp.w
+		});
+		
+		chartMain.append("g")
+		.classed("barChart", true);
 
-  //draw chart, initialized to all DRGs
-  var svg = d3.select("svg#chartMain");
+        chartMain.append("g")
+        .classed("keyLabels", true);
 
-  d3.csv('./data/national/DRG-All.csv', 
-    function(error, csv) {
-      svg.attr({
-        height: (csv.length * (barSpec.h + barSpec.spacing)) + chartSpec.topMargin
-      });
+		chartMain.call(drawChart, csv);
+	});
+}
 
-      //do the labels first, the chart's g will depend on the largest w
-      var labelG = svg.append('g')
-        .classed('labels', true)
-        .attr("transform", "translate(" + margin.l + "," + chartSpec.topMargin + ")");
+function getFilePath(drgCode){
+    var pathOut = './data/national/';
 
-      var labels = labelG.selectAll('text.barLabel')
-        .data(csv)
-        .enter()
-        .append('text')
-        .classed('barLabel', true)
+    switch (travelPath.length){
+        case 1 : break; //national level
+        case 2 : pathOut = pathOut + travelPath[1] + "/"; //state level
+                 break;
+        case 3 : pathOut = pathOut + travelPath[1] + "/" + travelPath[2] + "/"; //city level
+                 break;
+        case 4 : pathOut = pathOut + travelPath[1] + "/" + travelPath[2] + "/providers/" + travelPath[3] + "-DRGs.csv"; //provider level
+                 break;
+    }
+
+    //fileName part - provider level does not seperate by DRG
+    if(travelPath.length < 4){
+        pathOut = pathOut + "DRG-" + drgCode + ".csv";
+    }
+    console.log("path: " + pathOut);
+    return pathOut;
+}
+
+//this should handle all changes, since there isn't a lot of differences between them 
+function redrawChart(drgCode, travelLevel){
+    if (travelLevel !== undefined){
+        travelPath.push(travelLevel);
+    }
+    
+	d3.csv(getFilePath(drgCode), function(error, csv){
+		d3.select("#chartMain").call(drawChart, csv);
+	});
+}
+
+function drawChart(svg, data) {
+    svg.attr("height", (data.length * (barSpec.spacing + barSpec.h)) + chartSpec.insideOffset);
+
+    data.map(function(d){
+        d.avgCoveredCharges = Number(d.avgCoveredCharges);
+        d.averagePayments = Number(d.averagePayments);
+    });
+
+    var maxValue = d3.max(data, function(d){return d.avgCoveredCharges;});
+    var avgValue = d3.mean(data, function(d){return d.avgCoveredCharges;});
+    var xScale = d3.scale.linear().range([0, chartSpec.w]).domain([0, maxValue]);
+
+    console.log(xScale(maxValue));
+
+	//chart encompasses the bars, bar value labels, and lines of the chart, everything that starts at value 0
+	//the key labels are to the left of that so they do not count
+	var chart = svg.selectAll("g.barChart").attr("transform", "translate(0, " + chartSpec.insideOffset + ")");
+    var keyLabels = svg.selectAll("g.keyLabels").attr("transform", "translate(0, " + chartSpec.insideOffset + ")");
+
+    //this varies depending on where we are data-wise
+    var keyName;
+
+    switch (travelPath.length){
+        case 1 : keyName = 'state'; break;
+        case 2 : keyName = 'city'; break;
+        case 3 : keyName = 'providerName'; break;
+        case 4 : keyName = 'description'; break;
+    }
+
+	var rects = chart.selectAll("rect.bar").data(data, function(d){return d[keyName];});
+    var barValLabel = chart.selectAll("text.barValueLabel").data(data, function(d){return d[keyName];});
+	var labels = keyLabels.selectAll("text.barLabel").data(data, function(d){ return d[keyName];});
+
+    //add/edit max and avg lines
+    //if the two are the same, insert empty array so everything is removed
+    var avgArr = maxValue == avgValue ? [] : [avgValue];
+    var maxArr = maxValue == avgValue ? [] : [maxValue];
+    var avgLine = chart.selectAll("#avgLine").data(avgArr);
+    var avgLineLabel = svg.selectAll("#avgLineLabel").data(avgArr);
+    var maxLine = chart.selectAll("#maxLine").data(maxArr);
+    var maxLineLabel = svg.selectAll("#maxLineLabel").data(maxArr);
+
+	//remove old data
+	rects.exit()
+		.transition()
+		.duration(transitionSpeed)
+		.attr({
+			width: 0
+		}).remove();
+
+	//just remove it, no need for fanciness
+    barValLabel.exit().remove();
+
+	labels.exit()
+		.transition()
+		.duration(transitionSpeed)
+		.attr({
+			opacity: 0
+		}).remove();
+
+	//maxLine and avgLine do not show if maxValue and avgValue are the same (data is only one element)
+    avgLine.exit().remove();
+    avgLineLabel.exit().remove();
+    maxLine.exit().remove();
+    maxLineLabel.exit().remove();
+
+	//add/edit new labels - write them first as non visible to get the height and length, then position them based on their size
+	var newLabels = labels.enter().append("text")
+		.classed('barLabel', true)
         .attr({
           "font-size": chartSpec.label.size,
-          "dominant-baseline": "end",
+          "dominant-baseline": "middle",
           "text-anchor": "end",
           id : function(d,i){ return "lbl" + i;},
-          "fill" : chartSpec.label.color
+          "fill" : chartSpec.label.color,
+          opacity: 0
         })
-        .text(function(d){return d.state;});
+        .text(function(d){return d[keyName];});
 
-        //the labels are drawn first, then the height is used to calculate the y value
-        labelG.selectAll('text.barLabel')
-        .attr({
-          y: function (d, i) {return (i * (barSpec.h + barSpec.spacing)) + (this.getBBox().height);}
-        });
+	var chartStart = d3.max(labels[0], function(d){return d.getComputedTextLength();}) + 4;
+        
+    labels.transition()
+    .duration(transitionSpeed)
+    .delay(transitionSpeed)
+    .attr({
+      x: chartStart - 4,
+      y: function (d, i) {return (i * (barSpec.h + barSpec.spacing)) + (this.getBBox().height);}
+    });
+     
+    newLabels.transition()
+    .duration(transitionSpeed)
+    .delay(transitionSpeed)
+    .attr({
+		opacity: 1
+    });
 
-      //once the labels are made, calculate the longest label and add spacing to get where the bars should start
-      var chartStart = d3.max(labels[0], function(d){return d.getComputedTextLength();}) + 4;
-      var valMax = d3.max(csv, function(d){return d.avgCoveredCharges;});
-
-      d3.selectAll("text.barLabel")
-      .attr("x", chartStart - 4);
-
-      prevState.xScale = d3.scale.linear()
-        .domain([0, valMax])
-        .range([0, chartSpec.w]);
-
-      var bars = svg.append('g')
-        .classed('barChart', true)
-        .attr("transform", "translate(" + (margin.l + chartStart) + "," + chartSpec.topMargin + ")");
-
-      bars.selectAll('rect.bar')
-        .data(csv)
-        .enter()
-        .append('rect')
-        .classed('bar', true)
-        .attr({
-          x: 0,
-          y: function(d,i){return i * (barSpec.h + barSpec.spacing);},
-          height: barSpec.h,
-          width: function(d){return prevState.xScale(d.avgCoveredCharges);},
-          fill: barSpec.fill,
-          'opacity': 1,
-          id: function(d, i){ return "bar" + i;},
-          "barState": function(d){return d.state;}
-        })
+    //add/update rectangles
+	rects.enter().append("rect")
+		.classed('bar', true)
+		.attr({
+			x: chartStart,
+			y: function(d,i){ return i*(barSpec.h + barSpec.spacing); },
+			width: 0,
+			height : barSpec.h,
+			fill: barSpec.fill
+		})        
         .on('mouseover', function(d, i){
-          d3.select("#bar" + i)
+          d3.select(this)
             .attr("fill", barSpec.moFill);
 
           d3.select("#lbl" + i)
             .attr("fill", chartSpec.label.moColor);
         })
         .on('mouseout', function(d, i){
-          d3.select("#bar" + i)
+          d3.select(this)
             .attr("fill", barSpec.fill);
 
            d3.select("#lbl" + i)
             .attr("fill", chartSpec.label.color);
+        })
+        .on('click', function(d){
+            redrawChart($("#drgSelect").children(':selected').val(), keyName == "providerName" ? d.providerNationalID : d[keyName]);
         });
 
-      //bar value label
-      bars.selectAll("text.barValueLabel")
-        .data(csv)
-        .enter()
+	//update data
+	//grows the bar out to the width (inits to 0)
+	rects.transition()
+		.duration(transitionSpeed)
+		.delay(transitionSpeed)
+		.attr({
+			y: function(d,i){ return i*(barSpec.h + barSpec.spacing); },
+			width : function(d) { return xScale(d.avgCoveredCharges);}
+		});
+
+    //bar value labels
+    barValLabel.enter()
         .append('text')
         .classed('barValueLabel', true)
         .attr({
           "font-size": chartSpec.label.size,
-          "dominant-baseline": "end",
+          "dominant-baseline": "middle",
           "text-anchor": "end",
           id : function(d,i){ return "vLbl" + i;},
           "fill" : '#000',
-          'opacity': 1
-        })
-        .text(function(d){return '$' + commaSeparateNumber(d.avgCoveredCharges);});
+          'opacity': 1,
+          x: chartStart,
+          y: function (d, i) {return (barSpec.h/2) + (i * (barSpec.h + barSpec.spacing)) + 1;}
+        }).text("$0");
 
-        bars.selectAll('text.barValueLabel')
+    barValLabel.transition().duration(transitionSpeed).delay(transitionSpeed)
+         .tween("text", function(d) {
+            var i = d3.interpolate(this.textContent.replace(/\D/g,''), d.avgCoveredCharges);
+            return function(t) {
+                this.textContent = '$' + commaSeparateNumber(Math.round(i(t)));
+            };
+        })
         .attr({
-          x: function (d, i) {return prevState.xScale(d.avgCoveredCharges) - 2;},
-          y: function (d, i) {return (i * (barSpec.h + barSpec.spacing)) + (this.getBBox().height) + 1;}
+          x: function (d, i) {console.log(xScale(d.avgCoveredCharges)); return chartStart + xScale(d.avgCoveredCharges) - 5;},
+          y: function (d, i) {return (barSpec.h/2) + (i * (barSpec.h + barSpec.spacing)) + 1;}
         });
 
-      //the baseline is drawn after so it is over the bars
-      bars.append('line')
-        .attr({
-          x1:0,
-          x2:0,
-          y1:0,
-          y2: (csv.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing
-        })
-        .style("stroke", "#000")
-        .classed("baseLine", true);
-
-      //average line
-      var avg = d3.mean(csv, function(d){return d.avgCoveredCharges;});
-
-      bars.selectAll("#avgLine")
-      .data([avg])
-      .enter()
-      .append("line")
+    //avgLine add and update
+    avgLine.enter().append("line")
       .attr({
-        x1: function(d){return prevState.xScale(d);},
-        y1: 0  ,
-        x2: function(d){return prevState.xScale(d);},
-        y2: (csv.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing ,
-        "id" : 'avgLine',
-        'opacity': 1
+        x1: chartStart,
+        y1: 0,
+        x2: chartStart,
+        y2: (data.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing,
+        id: 'avgLine',
+        opacity: 1
       })
       .style({
         stroke: "#F3AF00",
@@ -158,138 +246,89 @@ function loadData(){
         'stroke-dasharray': ("6, 5")
       });
 
-      bars.selectAll("#avgLineLabel")
-      .data([avg])
-      .enter()
-      .append("text")
-        .attr({
-          "id" : "avgLineLabel",
-          "font-size": chartSpec.label.size,
-          "dominant-baseline": "end",
-          "text-anchor": "middle",
-          "fill" : '#000',
-          x: prevState.xScale(avg),
-          y: -5
-        })
-        .text('$' + commaSeparateNumber(Math.round(avg)));
-
-      //max line
-      bars.selectAll("#maxLine")
-      .data([valMax])
-      .enter()
-      .append("line")
+    avgLine.transition().duration(transitionSpeed).delay(transitionSpeed)
       .attr({
-        x1: function(d){return prevState.xScale(d);},
-        y1: 0  ,
-        x2: function(d){return prevState.xScale(d);},
-        y2: (csv.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing ,
-        "id" : 'maxLine',
-        'opacity': 1
+        x1: function(d){return chartStart + xScale(d);},
+        y1: 0,
+        x2: function(d){return chartStart + xScale(d);},
+        y2: (data.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing 
+      });
+
+    avgLine.moveToFront();
+
+    //avgLineLabel add and update
+    avgLineLabel.enter().append("text")
+        .attr({
+            x: chartStart,
+            y:  20,
+            id: "avgLineLabel",
+            'text-anchor' : "middle"
+        })
+        .text("$0");
+
+    avgLineLabel.transition().duration(transitionSpeed).delay(transitionSpeed)
+         .tween("text", function(d) {
+            var i = d3.interpolate(this.textContent.replace(/\D/g,''), avgValue);
+            return function(t) {
+                this.textContent = 'Average: $' + commaSeparateNumber(Math.round(i(t)));
+            };
+        })
+        .attr({
+          x: function (d, i) {return chartStart + xScale(avgValue);}
+        });
+
+    //maxLine add and update
+    maxLine.enter().append("line")
+      .attr({
+        x1: chartStart,
+        y1: 0,
+        x2: chartStart,
+        y2: (data.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing,
+        id: 'maxLine',
+        opacity: 1
       })
       .style({
-        stroke: "#000",
+        stroke: "#000000",
         'stroke-width': 1
       });
 
-      bars.selectAll("#maxLineLabel")
-      .data([valMax])
-      .enter()
-      .append("text")
+    maxLine.transition().duration(transitionSpeed).delay(transitionSpeed)
+      .attr({
+        x1: function(d){return chartStart + xScale(d);},
+        y1: 0,
+        x2: function(d){return chartStart + xScale(d);},
+        y2: (data.length * (barSpec.h + barSpec.spacing)) - barSpec.spacing 
+      });
+
+    //maxLineLabel add and update
+    maxLineLabel.enter().append("text")
         .attr({
-          "font-size": chartSpec.label.size,
-          "dominant-baseline": "end",
-          "text-anchor": "middle",
-          id : 'maxLineLabel',
-          "fill" : '#000',
-          x: prevState.xScale(valMax),
-          y: -5
+            x: chartStart,
+            y:  20,
+            id: "maxLineLabel",
+            'text-anchor' : "middle"
         })
-        .text('$' + commaSeparateNumber(Math.round(valMax)));
-    });
+        .text("$0");
+
+    maxLineLabel.transition().duration(transitionSpeed).delay(transitionSpeed)
+         .tween("text", function(d) {
+            var i = d3.interpolate(this.textContent.replace(/\D/g,''), maxValue);
+            return function(t) {
+                this.textContent = 'Max: $' + commaSeparateNumber(Math.round(i(t)));
+            };
+        })
+        .attr({
+          x: function (d, i) {return chartStart + xScale(maxValue);}
+        });
+   
 }
 
-function drillDown(){}
-
-function buildUp(){}
-
-  function commaSeparateNumber(val){
-    while (/(\d+)(\d{3})/.test(val.toString())){
-      val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-    }
-    return val;
-  }
-
-function dropListChange(value){
-  d3.csv('./data/national/DRG-' + value + '.csv',function(error, csv){
-    redrawNewData(csv);
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
   });
-}
+};
 
-function redrawNewData(newData){
-  var bars = d3.selectAll("rect.bar");
-  var newMax = d3.max(newData, function(d){return d.avgCoveredCharges;});
-  var newIndexes = newData.map(function(d){ return d.state;});
-  var oldIndexes = (bars.data()).map(function(d){ return d.state;});
-
-  d3.selectAll("rect.bar, text.barValueLabel, text#maxLineLabel, line#maxLine, text#avgLineLabel, line#avgLine").remove();
-
-  
-
-}
-
-function reScale(max){
-  var def = $.Deferred();
-
-  var newX = d3.scale.linear()
-    .domain([0, max])
-    .range([0, chartSpec.w]); 
-
-  d3.selectAll("rect.bar")
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      width : function(d) {return newX(d.avgCoveredCharges);}
-    });
-
-  d3.select("#maxLine")
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      x1 : function(d) {return newX(d);},
-      x2 : function(d) {return newX(d);}
-    });
-
-  d3.select("#maxLineLabel")
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      x : function(d) {return newX(d);}
-    });
-
-  d3.select("#avgLine")
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      x1 : function(d) {return newX(d);},
-      x2 : function(d) {return newX(d);}
-    });
-
-  d3.select("#avgLineLabel")
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      x : function(d) {return newX(d);}
-    });
-
-  d3.selectAll('text.barValueLabel')
-    .transition()
-    .duration(transitionSpeed)
-    .attr({
-      x: function (d, i) {return newX(d.avgCoveredCharges) - 2;}
-    })
-    .each('end',function(){def.resolve();});
-
-  prevState.xScale = newX;
-
-  return def.promise();
+function commaSeparateNumber(val){
+  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
